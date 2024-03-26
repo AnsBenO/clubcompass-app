25f481daf00792c68bd47ae814a65d34ed663b7c
AnsBenO <ans.benomar@gmail.com>
2024-03-26 00:09:11 +0000

    formated file

--- src/main/java/mvc/spring/web/mappers/ClubMapper.java
+++ src/main/java/mvc/spring/web/mappers/ClubMapper.java
@@ -8,31 +8,31 @@ import mvc.spring.web.dto.ClubDto;
 import mvc.spring.web.models.Club;
 
 public class ClubMapper {
-    public static ClubDto mapToClubDto(@NotNull Club club) {
-        return ClubDto.builder()
-                .id(club.getId())
-                .title(club.getTitle())
-                .photoUrl(club.getPhotoUrl())
-                .description(club.getDescription())
-                .createdAt(club.getCreatedAt())
-                .updatedAt(club.getUpdatedAt())
-                .events(club.getEvents().stream()
-                        .map((event) -> EventMapper.mapToEventDto(event))
-                        .collect(Collectors.toList()))
-                .build();
-    }
+        public static ClubDto mapToClubDto(@NotNull Club club) {
+                return ClubDto.builder()
+                                .id(club.getId())
+                                .title(club.getTitle())
+                                .photoUrl(club.getPhotoUrl())
+                                .description(club.getDescription())
+                                .createdAt(club.getCreatedAt())
+                                .updatedAt(club.getUpdatedAt())
+                                .events(club.getEvents().stream()
+                                                .map((event) -> EventMapper.mapToEventDto(event))
+                                                .collect(Collectors.toList()))
+                                .build();
+        }
 
-    public static Club mapToClubEntity(@NotNull ClubDto clubDto) {
-        return Club.builder()
-                .id(clubDto.getId())
-                .title(clubDto.getTitle())
-                .photoUrl(clubDto.getPhotoUrl())
-                .description(clubDto.getDescription())
-                .createdAt(clubDto.getCreatedAt())
-                .updatedAt(clubDto.getUpdatedAt())
-                .events(clubDto.getEvents().stream()
-                        .map((event) -> EventMapper.mapToEvent(event))
-                        .collect(Collectors.toList()))
-                .build();
-    }
+        public static Club mapToClubEntity(@NotNull ClubDto clubDto) {
+                return Club.builder()
+                                .id(clubDto.getId())
+                                .title(clubDto.getTitle())
+                                .photoUrl(clubDto.getPhotoUrl())
+                                .description(clubDto.getDescription())
+                                .createdAt(clubDto.getCreatedAt())
+                                .updatedAt(clubDto.getUpdatedAt())
+                                .events(clubDto.getEvents().stream()
+                                                .map((event) -> EventMapper.mapToEvent(event))
+                                                .collect(Collectors.toList()))
+                                .build();
+        }
 }
